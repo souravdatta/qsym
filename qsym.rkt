2a057f4f0a27025d44480285858cfe4423ebbeb0
Sourav Datta <soura.jagat@gmail.com>
2024-03-08 13:42:55 +0530

    Create qsym.rkt
    Initial creation with few built-in functions to build circuits.

---
+++ qsym.rkt
@@ -0,0 +1,150 @@
+#lang racket
+
+;; Copyright Sourav Datta (soura.jagat@gmail.com)
+;; You should have received a copy of the LICENSE along with this code
+;; in the repository. If not refer to
+;; https://github.com/souravdatta/qsym/blob/main/LICENSE
+
+
+(require math/matrix)
+(require math/array)
+
+(define (e-equal? x y)
+  (< (abs (- x y)) 0.00001))
+
+(define (qubit a b)
+  (if (e-equal? (+ (* (magnitude a)
+                      (magnitude a))
+                   (* (magnitude b)
+                      (magnitude b)))
+                1.0)
+      (col-matrix [a b])
+      (error "Cannot create qubit, bad probabilities")))
+
+(define q0 (qubit 1 0))
+
+(define ((apply-op op-matrix) q)
+  (matrix* op-matrix q))
+
+(define pauli-x (matrix [[0 1]
+                         [1 0]]))
+
+(define X (apply-op pauli-x))
+
+(define pauli-z (matrix [[1 0]
+                         [0 -1]]))
+
+(define Z (apply-op pauli-z))
+
+(define pauli-y (matrix [[0 0-i]
+                         [0+i 0]]))
+
+(define Y (apply-op pauli-y))
+
+(define h-factor (/ 1.0 (sqrt 2)))
+
+(define hadamard (matrix [[h-factor h-factor]
+                          [h-factor (- h-factor)]]))
+
+(define H (apply-op hadamard))
+
+;; -----
+
+(define (bits-of-len n)
+  (cond
+    ((< n 1) (error "Invalid bits length, must be >= 1"))
+    ((= n 1) '((0) (1)))
+    (else (let ([lower-bits (bits-of-len (- n 1))])
+            (for*/list ([i (range 2)]
+                        [x lower-bits])
+              (cons i x))))))
+
+(define (range-map start probs obs roll i n)
+  (if (= i n)
+      (error "No observation from circuit - invalid state")
+      (if (= (list-ref probs i) 0)
+          (range-map start
+                     probs
+                     obs
+                     roll
+                     (+ i 1)
+                     n)
+          (if (and (>= roll start)
+                   (< roll (+ start (list-ref probs i))))
+              (list-ref obs i)
+              (range-map (+ start (list-ref probs i)) ; next start
+                         probs
+                         obs
+                         roll
+                         (+ i 1)            ; next observation to match
+                         n)))))
+
+(define (measure-mat m)
+  (let* ([len (matrix-num-rows m)]
+         [blen (exact-round (log len 2))]
+         [obs (bits-of-len blen)]
+         [ampls (matrix->list m)]
+         [scaled-probs (map (λ (x) (*  x x 100)) ampls)]
+         [roll (random 100)])
+    (range-map 0 scaled-probs obs roll 0 len)))
+
+(define (counts q #:shots [shots 1024])
+  (let ([results (make-hash)])
+    (for ([i (range shots)])
+      (let ([obs (measure-mat q)])
+        (if (hash-has-key? results obs)
+            (hash-set! results obs (+ (hash-ref results obs) 1))
+            (hash-set! results obs 1))))
+    results))
+
+(define (tensor* m1 m2)
+  (define (mat->list m1 m2)
+    (matrix->list*
+     (matrix-map (λ (x)
+                   (matrix->list*
+                    (matrix-map (λ (y)
+                                  (* x y))
+                                m2)))
+                 m1)))
+
+  (define (list->mat m)
+    (let ([rows '()])
+      (for ([row m])
+        (for ([i (range (length (first row)))])
+          (let ([row-line '()])
+            (for ([m row])
+              (set! row-line (append row-line (list-ref m i))))
+            (set! rows (append rows (list row-line))))))
+      rows))
+  (let* ([m (mat->list m1 m2)]
+         [l (list->mat m)])
+    (list*->matrix l)))
+
+(define (t* . qbits)
+  (let ([n (length qbits)])
+    (cond
+      ((< n 1) (error "No qubits given"))
+      ((= n 1) (first qbits))
+      (else (tensor* (first qbits)
+                     (apply t* (rest qbits)))))))
+
+(define cnot-gate (matrix [[1 0 0 0]
+                           [0 1 0 0]
+                           [0 0 0 1]
+                           [0 0 1 0]]))
+
+(define CX (apply-op cnot-gate))
+
+(define (I n)
+  (identity-matrix n))
+
+(define (G* . ms)
+  (apply-op (apply t* ms)))
+
+(define (qG* . ms)
+  (apply-op (apply t* (reverse ms))))
+
+(define h2 (G* hadamard hadamard))
+
+(provide (all-defined-out))
+
