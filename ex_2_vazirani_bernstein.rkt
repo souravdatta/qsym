6a8b81c95ae680cf1cf26fe3d8f6bd796dc1ded3
Sourav Datta <soura.jagat@gmail.com>
2024-06-25 11:46:47 +0530

    Update ex_2_vazirani_bernstein.rkt with qlang implementation

--- ex_2_vazirani_bernstein.rkt
+++ ex_2_vazirani_bernstein.rkt
@@ -6,6 +6,8 @@
 ;; https://github.com/souravdatta/qsym/blob/main/LICENSE
 
 (require "qsym.rkt")
+(require "qlang.rkt")
+
 
 ;; Oracle for 10011
 (define oracle (gate-matrix 6
@@ -36,3 +38,39 @@
 
 (plot-histogram
  (counts r #:shots 2000)) ;; 110011 with close 100% probability
+
+;; Using Qlang
+(define c (def-circuit 6
+              (def-layer (x 5))
+              (def-layer
+                (h 0)
+                (h 1)
+                (h 2)
+                (h 3)
+                (h 4)
+                (h 5))
+              ;; begin oracle
+              (def-layer (cx 0 5))
+              (def-layer (cx 1 5))
+              (def-layer (cx 4 5))
+              ;; end oracle
+              (def-layer
+                (h 0)
+                (h 1)
+                (h 2)
+                (h 3)
+                (h 4)
+                (h 5))))
+
+(define sim (sv-simulator c))
+(plot-histogram
+ (counts (sim (qubits 6))))
+
+(draw-circuit c)
+;; 
+;; |  i              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
+;; |  i              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
+;; |  i              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
+;; |  i              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
+;; |  i              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
+;; |  x              | -> |  h              | -> |< (cx 0 5) >| -> |< (cx 1 5) >| -> |< (cx 4 5) >| -> |  h              | -> 
